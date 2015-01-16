896a748f7ca386b36ff755689ab9a5c36b82ebce
Sylvain Hallé <shalle@acm.org>
2015-01-16 10:11:53 -0500

    Removed doc from project (can be auto-generated by Ant)

--- Source/MapReduce/doc/html/interfaceca_1_1uqac_1_1dim_1_1mapreduce_1_1Mapper_3_01K_00_01V_01_4.js
+++
@@ -1,4 +0,0 @@
-var interfaceca_1_1uqac_1_1dim_1_1mapreduce_1_1Mapper_3_01K_00_01V_01_4 =
-[
-    [ "map", "interfaceca_1_1uqac_1_1dim_1_1mapreduce_1_1Mapper_3_01K_00_01V_01_4.html#ab4e3884595b354a9afd7d997ce59d9d5", null ]
-];
--- Source/MapReduce/doc/html/namespaces.js
+++
@@ -1,4 +0,0 @@
-var namespaces =
-[
-    [ "ca.uqac.dim.mapreduce", "namespaceca_1_1uqac_1_1dim_1_1mapreduce.html", "namespaceca_1_1uqac_1_1dim_1_1mapreduce" ]
-];
